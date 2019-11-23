7a565966f9c9fdbf9d861f81a6078fc17a4cbcde
ramonamarti <ramonamarti@gmx.net>
2019-11-23 08:52:54 +0100

    third changes test and app works on its basics

--- todoliste/src/main/java/gruppe3/todoliste/TodolisteApplication.java
+++ todoliste/src/main/java/gruppe3/todoliste/TodolisteApplication.java
@@ -1,11 +1,8 @@
 package gruppe3.todoliste;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootApplication
 public class TodolisteApplication {
 
--- todoliste/src/test/java/gruppe3/todoliste/TodolisteApplicationTests.java
+++ todoliste/src/test/java/gruppe3/todoliste/TodolisteApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class TodolisteApplicationTests {
+public class TodolisteApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
