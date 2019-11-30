eb92af798ad2921993946074c639c1ecbdfda3d2
eMZetta <michi.zihlmann@gmx.net>
2019-11-30 13:23:54 +0100

    adaption ListService

--- todoliste/src/main/java/gruppe3/todoliste/service/ListService.java
+++ todoliste/src/main/java/gruppe3/todoliste/service/ListService.java
@@ -8,7 +8,8 @@ import javax.validation.Valid;
 import java.util.Optional;
 
 /***
- * service to add a module, get a module with its id or remove a module
+ * dashboard to show all entries
+ * Author: Michi
  */
 @Service
 public class ListService {
@@ -19,8 +20,8 @@ public class ListService {
     }
 
     /**
-     * to add and save a module in the table module
-     * @param l: module to save
+     * to add and save a entry
+     * @param l: entry to save
      * @return id of the entry
      */
     public Long addList(@Valid List l){
@@ -28,9 +29,9 @@ public class ListService {
     }
 
     /**
-     * to get a module form the table module with a id
-     * @param id: id of a module
-     * @return module with the correct id from the table module
+     * get a entry with a id
+     * @param id: id of a entry
+     * @return entry with the correct id
      */
     public List getList(Long id){
         List module = new List();
@@ -44,8 +45,8 @@ public class ListService {
     }
 
     /**
-     * to delete a module from the table module
-     * @param id: id of a module
+     * to delete a entry
+     * @param id: id of a entry
      */
     public void removeList(Long id){
         if(id != null){
