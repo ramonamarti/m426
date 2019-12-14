d8d62d2190bfca47a8f8f4b4e1f231e1fd2135a2
ramonamarti <ramonamarti@gmx.net>
2019-12-14 13:30:32 +0100

    refactoring and tests

--- todoliste/src/test/java/gruppe3/todoliste/TodolisteApplicationTests.java
+++ todoliste/src/test/java/gruppe3/todoliste/TodolisteApplicationTests.java
@@ -1,28 +1,17 @@
 package gruppe3.todoliste;
 
-import gruppe3.todoliste.controller.PersonController;
 import gruppe3.todoliste.model.List;
 import gruppe3.todoliste.model.Login;
 import gruppe3.todoliste.model.Person;
 import gruppe3.todoliste.repos.ListRepository;
 import gruppe3.todoliste.repos.LoginRepository;
 import gruppe3.todoliste.repos.PersonRepository;
-import gruppe3.todoliste.service.ListService;
-import gruppe3.todoliste.service.LoginService;
-import gruppe3.todoliste.service.PersonService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.ui.Model;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
-
-import java.util.Enumeration;
 
 import static junit.framework.TestCase.*;
 
