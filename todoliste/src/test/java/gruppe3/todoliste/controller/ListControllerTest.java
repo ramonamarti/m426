3ed9dd75754364123172f4bec2cfe860cba5ef6c
eMZetta <michi.zihlmann@gmx.net>
2019-12-14 13:04:50 +0100

    add comment to ListControllerTest

--- todoliste/src/test/java/gruppe3/todoliste/controller/ListControllerTest.java
+++ todoliste/src/test/java/gruppe3/todoliste/controller/ListControllerTest.java
@@ -9,16 +9,21 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+/**
+ * Testclass tests if the attribut after renaming to LIST_SERVICE still works.
+ * If the test result ist green, LIST_SERVICE is not null.
+ * @Author: Michael Zihlmann
+ */
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 class ListControllerTest {
 
     @Mock
-    private ListService listService;
+    private ListService LIST_SERVICE;
 
     @InjectMocks
-    private ListController listController = new ListController(listService);
+    private ListController listController = new ListController(LIST_SERVICE);
 
     @Test
     void todo() {
